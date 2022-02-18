da98973de68fae73e86032e81ac81526d283acee
XR1140 <xr1140@gmail.com>
2022-02-18 13:36:39 +0200

    init msg

--- classes_test2.py
+++ classes_test2.py
@@ -10,37 +10,37 @@ def gen_string(nr: int = 5) -> str:
     return ''.join(random.choices(string.ascii_lowercase, k=nr))
 
 
-class DummyFTP:
+def gen_number(nr: int = 5) -> str:
+    # return ''.join(random.choice(string.ascii_lowercase) for i in range(nr))
+    return ''.join(random.choices(string.digits, k=nr))
+
+
+class Parent:
     def __init__(self, name):
         self.name = name
 
-    def dummy_cwd(self):
+    def dummy_name(self):
         print(' '.join(self.name for i in range(3)))
 
-class Client:
-    # daca e definit aici in toate instantele copil are aceiasi valoare
-    copil = DummyFTP(gen_string())
 
-    def __init__(self, name):
-        self.name = name
-        # daca e definit aici copil este diferit in fiecare instanta
-        # self.copil = DummyFTP(gen_string())
+class Child(Parent):
 
-    def what_name(self):
+    def __init__(self, id, name):
+        super().__init__(name)
+        self.id = id
+
+    def what_id(self):
         # print(self.copil.name)
-        self.copil.dummy_cwd()
+        super().dummy_name()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    p1 = Parent("parent")
+    c1 = Child("24234", "child")
 
-    c1 = Client('nume1')
-    c2 = Client('nume2')
-
-    c1.what_name()
-    c1.copil = None
-
-    print(Client.__dict__)
+    print(p1.__dict__)
     print(c1.__dict__)
-    # print(c2.__dict__)
-    # c2.what_name()
+
+    p1.dummy_name()
+    c1.what_id()
